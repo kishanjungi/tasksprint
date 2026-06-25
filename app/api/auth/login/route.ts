b5fde6eaa825b41be45aba9463d4e4c912a1d073
kishanjungi <kishanjungi5@gmail.com>
2026-06-25 13:31:01 +0530

    Implemented JWT authentication and login API

---
+++ app/api/auth/login/route.ts
@@ -0,0 +1,69 @@
+import { NextRequest, NextResponse } from "next/server";
+import bcrypt from "bcryptjs";
+
+import { prisma } from "@/lib/prisma";
+import { generateToken } from "@/lib/jwt";
+
+export async function POST(req: NextRequest) {
+  try {
+    const body = await req.json();
+
+    const { email, password } = body;
+
+    const user = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (!user) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "Invalid credentials",
+        },
+        { status: 401 }
+      );
+    }
+
+    const validPassword = await bcrypt.compare(
+      password,
+      user.password
+    );
+
+    if (!validPassword) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "Invalid credentials",
+        },
+        { status: 401 }
+      );
+    }
+
+    const token = generateToken(
+      user.id,
+      user.email
+    );
+
+    return NextResponse.json({
+      success: true,
+      token,
+      user: {
+        id: user.id,
+        username: user.username,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+
+    return NextResponse.json(
+      {
+        success: false,
+        message: "Server Error",
+      },
+      { status: 500 }
+    );
+  }
+}
